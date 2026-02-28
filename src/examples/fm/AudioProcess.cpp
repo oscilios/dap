5b100b5b93ffd2e740b3bc920f90359ed3b67427
eaylon <eaylon@oscilios.com>
2026-02-28 20:04:10 +0100

    add fm example

---
+++ src/examples/fm/AudioProcess.cpp
@@ -0,0 +1,54 @@
+#include "AudioProcess.h"
+#include "audioio/AudioBusFactory.h"
+#include "base/SystemCommon.h"
+
+using fm::AudioProcess;
+using dap::audioio::AudioBusFactory;
+
+AudioProcess::AudioProcess(int32_t outputId,
+                           size_t outputChannelCount,
+                           size_t bufferSize,
+                           float sampleRate)
+: m_outputBus(
+      AudioBusFactory::create(dap::audioio::Scope::Output, outputId, bufferSize, sampleRate))
+, m_outputChannelCount(outputChannelCount)
+, m_synth(bufferSize, sampleRate)
+{
+    m_synth.setGain(0.25f);
+    m_synth.setCarrierFreq(440.0f);
+    m_synth.setModFreq(110.0f);
+    m_synth.setModIndex(5.25f);
+}
+
+AudioProcess::~AudioProcess() = default;
+bool AudioProcess::process()
+{
+    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+
+    m_synth.process();
+    const auto& buf = m_synth.output();
+    for (size_t ch = 0; ch < m_outputChannelCount; ++ch)
+    {
+        std::memcpy(m_outputs[ch], buf.channel(0).data(), sizeof(float) * buf.channelSize());
+    }
+    return true;
+}
+void AudioProcess::setInputs(float const* const* const)
+{
+}
+void AudioProcess::setOutputs(float** outputs)
+{
+    m_outputs = outputs;
+}
+bool AudioProcess::start()
+{
+    m_outputBus->setAudioProcess(this);
+    return m_outputBus->start();
+}
+bool AudioProcess::stop()
+{
+    const auto result = m_outputBus->stop();
+    m_outputBus->removeAudioProcess();
+    return result;
+}
---
+++ src/examples/fm/FMSynth.h
@@ -0,0 +1,95 @@
+#pragma once
+#include "crtp/nodes/Processor.h"
+#include "dsp/Oscillator.h"
+#include "dsp/Smoother.h"
+#include "fastmath/AudioBuffer.h"
+#include <cstddef>
+
+namespace fm
+{
+    using dap::crtp::processor;
+    using dap::operator""_s;
+    using scalar_t     = float;
+    using samplerate_t = scalar_t;
+    class Synth;
+}
+
+class fm::Synth final
+{
+    struct Graph
+    {
+    private:
+        // a smoothed control
+        using control_t = decltype(
+            processor<dap::dsp::FixedSmoother<scalar_t, 512>>::with_inputs<scalar_t>::named(
+                "value"_s));
+
+        // alias for oscillator shapes
+        using shape_t = dap::dsp::OscillatorFunctions::Shape;
+
+        // define an oscillator and its input names
+        template <typename Amp, typename Freq, typename Ph>
+        using osc_t =
+            decltype(processor<dap::dsp::Oscillator<scalar_t>>::
+                         with_inputs<Amp, Freq, Ph, samplerate_t, shape_t>::named("gain"_s,
+                                                                                  "frequency"_s,
+                                                                                  "phase"_s,
+                                                                                  "samplerate"_s,
+                                                                                  "shape"_s));
+
+        // oscillator where gain, frequency and phase are smoothed controls
+        using control_osc_t = osc_t<control_t, control_t, control_t>;
+        // modulated control: the result of adding a control with an oscillator
+        using mod_control_t = decltype(control_t{} + control_osc_t{});
+        // an fm oscillator
+        using fm_t = osc_t<control_t, mod_control_t, control_t>;
+
+    public:
+        using type  = osc_t<control_t, mod_control_t, control_t>;
+        using shape = shape_t;
+    };
+
+    using buffer_t = dap::fastmath::AudioBuffer<float>;
+
+    buffer_t m_output;
+    Graph::type m_graph;
+
+    auto& modulator()
+    {
+        return m_graph.input("frequency"_s).input("y"_s);
+    }
+
+public:
+    Synth(size_t bufferSize, float samplerate);
+
+    void process()
+    {
+        for (auto& x : m_output.channel(0))
+        {
+            x = m_graph();
+        }
+    }
+
+    const buffer_t& output() const
+    {
+        return m_output;
+    }
+
+    void setCarrierFreq(float hz)
+    {
+        m_graph.input("frequency"_s).input("x"_s).input("value"_s) = hz;
+    }
+    void setModFreq(float hz)
+    {
+        modulator().input("frequency"_s).input("value"_s) = hz;
+    }
+    void setModIndex(float idx)
+    {
+        auto g = idx * modulator().input("frequency"_s).input("value"_s);
+        modulator().input("gain"_s).input("value"_s) = g;
+    }
+    void setGain(float g)
+    {
+        m_graph.input("gain"_s).input("value"_s) = g;
+    }
+};
